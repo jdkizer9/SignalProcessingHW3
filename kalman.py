33245a688c6f3e57553dd6b33a35da14b209fe90
James <james.kizer@gmail.com>
2014-12-14 19:33:58 -0500

    working kalman filter

---
+++ kalman.py
@@ -0,0 +1,158 @@
+import numpy as np
+import simulation
+import matplotlib.pyplot as plt
+import math
+
+
+
+
+def kalmanFilter(T=1, variance_w=.3, variance_v=1000, epsilon=.00001, initialState=[0., 0., 0., 0.], sensorProbability=1.):
+
+
+	trueTrajectoryArray, rawMeasurementsArray, trueTrajectoryMatrix, z_T, z_velocity = simulation.generateDataWithMatrices()
+
+	z = np.array([m.T for m in z_T])
+	#A = 4x4
+	#B = 4x2
+	#C = 2x4
+	#P = 4x4
+	#Q = 4x4
+	#R = 2x2
+	#K = 4x2
+	#U = 2x1
+	#X = 4x1
+	#Z = 2x1
+		# intial parameters
+	# n_iter = 50
+	# sz = (n_iter,) # size of array
+	# x = -0.37727 # truth value (typo in example at top of p. 13 calls this z)
+	# z = numpy.random.normal(x,0.1,size=sz) # observations (normal about x, sigma=0.1)
+
+	#Constant Matrices
+	A = np.matrix([[1., T, 0., 0.], [0., 1., 0., 0.], [0., 0., 1., T], [0., 0., 0., 1.]])
+	B = np.matrix([[.5*T**2, 0], [T, 0.], [0., .5*T**2], [0, T]])
+	C = np.matrix([[1., 0., 0., 0.], [0., 0., 1., 0.]])
+
+	K = np.empty(len(z), dtype=np.dtype(object))
+
+	Pminus = np.empty(len(z), dtype=np.dtype(object))
+	P = np.empty(len(z), dtype=np.dtype(object))
+	
+	Q = np.identity(4)*variance_w
+	R = np.identity(2)*variance_v
+
+	u = np.array([m.T for m in np.matrix(np.random.multivariate_normal([0,0], np.identity(2)*variance_w, len(z)))])
+
+	xhat = np.empty(len(z), dtype=np.dtype(object))
+	xhatminus = np.empty(len(z), dtype=np.dtype(object))
+	
+	# intial guesses
+	xhat[0] = np.matrix(initialState).T
+	P[0] = np.matrix([[epsilon, 0., 0., 0.], [0., epsilon, 0., 0.], [0., 0., epsilon, 0.], [0., 0., 0., epsilon]])
+
+	I = np.identity(4)
+
+	for k in range(1,len(z)):
+		# time update
+		assert(A.shape == (4,4))
+		#print(xhat[k-1].shape)
+		assert(xhat[k-1].shape == (4,1))
+		assert(B.shape == (4,2))
+		assert(u[k-1].shape == (2,1))
+
+		xhatminus[k] = A*xhat[k-1] + B*u[k-1]
+
+		assert(xhatminus[k].shape == (4,1))
+
+		#print(P[k-1].shape)
+		assert(P[k-1].shape == (4,4))
+		assert(Q.shape == (4,4))		
+
+		Pminus[k] = A*P[k-1]*A.T + Q
+
+		assert(Pminus[k].shape == (4,4))
+
+
+		# measurement update
+
+		assert(C.shape == (2,4))
+		assert(R.shape == (2,2))
+
+		K[k] = Pminus[k]*C.T * (C*Pminus[k]*C.T + R).getI()
+
+		assert(K[k].shape == (4,2))
+
+		#print(z[k].shape)
+		assert(z[k].shape == (2,1))
+
+		##implement imperfect sensor here
+
+		xhat[k] = xhatminus[k] + K[k]*(z[k]-C*xhatminus[k])
+
+		assert(xhat[k].shape == (4,1))
+
+		P[k] = (I-K[k]*C)*Pminus[k]
+
+		assert(P[k].shape == (4,4))
+
+	#print(xhat)
+
+	xhat_a = np.zeros((len(xhat),4))
+	print(xhat[0].shape)
+	for i in range(len(xhat)):
+		#print(xhat[0].shape)
+		xhat_a[i] = xhat[i].T.getA()
+
+	def computeEstimationError(truth, estimate):
+		print(truth.shape)
+		print(estimate.shape)
+		assert(truth.shape == (4,1))
+		assert(estimate.shape == (4,1))
+		xTrue = truth[0,0]
+		yTrue = truth[2,0]
+		xEstimate = estimate[0,0]
+		yEstimate = estimate[2,0]
+
+		# print(truth)
+		# print(estimate)
+
+		return math.sqrt((xEstimate - xTrue)**2 + (yEstimate - yTrue)**2)
+
+	def computeMeasurementError(truth, measurement):
+		assert(truth.shape == (4,1))
+		assert(measurement.shape == (2,1))
+
+		xTrue = truth[0,0]
+		yTrue = truth[2,0]
+		xMeasurement = measurement[0,0]
+		yMeasurement = measurement[1,0]
+
+		return math.sqrt((xMeasurement - xTrue)**2 + (yMeasurement - yTrue)**2)
+
+	estimationError = [computeEstimationError(trueTrajectoryMatrix[i].T, xhat[i]) for i in range(len(xhat))]
+	measurementError = [computeMeasurementError(trueTrajectoryMatrix[i].T, z[i]) for i in range(len(xhat))]
+
+	plt.figure()
+	plt.plot(trueTrajectoryArray[:,0], trueTrajectoryArray[:,2])
+	plt.plot(rawMeasurementsArray[:,0], rawMeasurementsArray[:,1])
+	plt.plot(xhat_a[:,0], xhat_a[:,2])
+
+
+	plt.figure()
+
+	plt.plot(list(range(len(estimationError))), np.zeros(len(estimationError)))
+	plt.plot(list(range(len(estimationError))), estimationError)
+	plt.plot(list(range(len(measurementError))), measurementError)
+
+	# plt.figure()
+	# plt.plot(list(range(len(groundTruthData))), groundTruthData[:,1])
+	# plt.plot(list(range(len(groundTruthData))),  noisyMeasurement[:,1])
+
+	# plt.figure()
+	# plt.plot(list(range(len(groundTruthData))), groundTruthData[:,3])
+	# plt.plot(list(range(len(groundTruthData))),  noisyMeasurement[:,3])
+
+	plt.show()
+
+if __name__ == "__main__":
+	kalmanFilter()
--- simulation.py
+++ simulation.py
@@ -28,10 +28,10 @@ def generateGroundTruth():
 	baseK = 200
 	P0x = groundTruth[baseK][0]
 	V0x = groundTruth[baseK][1]
-	A0x = 2
+	A0x = 2.
 	P0y = groundTruth[baseK][2]
 	V0y = groundTruth[baseK][3]
-	A0y = 2
+	A0y = 2.
 
 	for k in range (1, 101):
 		##[px, vx, py, vy]
@@ -63,9 +63,90 @@ def generateGroundTruth():
 
 	return groundTruth
 
+def generateDataWithMatrices(T=1., measurementNoise=([0,0], [[1000,0],[0,1000]])):
+
+	##len 501 array of 4x1 Matrices
+
+
+	xhat = np.empty(501, dtype=np.dtype(object))
+	xhatminus = np.empty(501)
+
+	xhat[0] = np.matrix([0., 300., 0., 0.])
+	#print(xhat[0])
+
+	A = np.matrix([[1., T, 0., 0.], [0., 1., 0., 0.], [0., 0., 1., T], [0., 0., 0., 1.]])
+	B = np.matrix([[.5*T**2, 0], [T, 0.], [0., .5*T**2], [0, T]])
+
+	u = np.matrix([0., 0.])
+
+	for i in range(1, 201):
+		#print(A)
+		#print(xhat[i-1].T)
+		xhat[i] = (A*xhat[i-1].T + B*u.T).T
+		#print(xhat[i])
+
+
+	u = np.matrix([2., 2.])
+	for i in range(201, 301):
+		xhat[i] = (A*xhat[i-1].T + B*u.T).T
+
+	# print(xhat[200])
+	# print(A*xhat[200].T)
+	# print(B*u.T)
+	# print(xhat[201])
+
+	u = np.matrix([0., 0.])
+	# print(xhat[300])
+	# print(type(xhat[300]))
+	xhat[300][0,1] = 0
+	xhat[300][0,3] = 300
+	for i in range(301, 501):
+		xhat[i] = (A*xhat[i-1].T + B*u.T).T
+
+
+	z = np.empty(len(xhat), dtype=np.dtype(object))
+	z[0] = np.matrix([xhat[0][0,0], xhat[0][0,2]])
+	C = np.matrix([[1., 0., 0., 0.], [0., 0., 1., 0.]])
+	for i in range(1, len(xhat)):
+		#print(C.shape)
+		#print(xhat[i].T.shape)
+		#print(C*xhat[i].T)
+		#print(np.matrix(np.random.multivariate_normal(*measurementNoise)).T)
+		z[i] = (C*xhat[i].T + np.matrix(np.random.multivariate_normal(*measurementNoise)).T).T
+		#print(z[i].shape)
+
+
+	z_velocity = np.empty(len(z), dtype=np.dtype(object))
+	z_velocity[0] = np.matrix([z[0][0,0], 0., z[0][0,1], 0.])
+	for i in range(1, len(z)):
+		z_velocity[i] = np.matrix([z[i][0,0], z[i][0,0] - z[i-1][0,0], z[i][0,1], z[i][0,1] - z[i-1][0,1]])
+		# z_velocity[i][0,0] = z[i][0,0]
+		# z_velocity[i][0,1] = z[i][0] - z[i-1][0]
+		# z_velocity[i][0,2] = z[i][0,1]
+		# z_velocity[i][0,3] = z[i][0,1] - z[i-1][0,1]
+
+	xhat_a = np.zeros((len(xhat),4))
+	for i in range(len(xhat)):
+		#print(xhat[0].shape)
+		xhat_a[i] = xhat[i].getA()
+
+	print(len(z))
+	z_a = np.zeros((len(z),2))
+	for i in range(len(z)):
+		#print(z[i])
+		#print(z[i].shape)
+		z_a[i] = z[i].getA()
+
+	return xhat_a, z_a, xhat, z, z_velocity
+
 def plotGroundTruth():
 
 	groundTruthData = generateGroundTruth()
+	groundTruthData_m, measuredData = generateDataWithMatrices()
+	groundTruthData_mo = np.zeros((len(groundTruthData_m),4))
+	for i in range(len(groundTruthData_m)):
+		groundTruthData_mo[i] = groundTruthData_m[i].getA()
+
 	##print(groundTruthData) 
 
 	#select column 0
@@ -74,13 +155,16 @@ def plotGroundTruth():
 	#select column 2
 	groundTruthPy = groundTruthData[:,2]
 
-	print(groundTruthData[:,0])
-	print(groundTruthData[:,[0,2]])
+	# print(groundTruthData[:,0])
+	# print(groundTruthData[:,[0,2]])
 
 
 	plt.figure()
 
-	plt.plot(groundTruthPx, groundTruthPy)
+	#print(groundTruthData)
+	#print(groundTruthData_m)
+	plt.plot(groundTruthData[:,0], groundTruthData[:,2])
+	plt.plot(groundTruthData_mo[:,0], groundTruthData_mo[:,2])
 	plt.show()
 
 	# pylab.figure()
@@ -93,9 +177,9 @@ def plotGroundTruth():
 	# pylab.xlabel('Iteration')
 	# pylab.ylabel('Voltage')
 
-def generateNoisyMeasurementsFromGroundTruthData(groundTruthData, mean=[0,0], covariance=[[1000,0],[0,1000]]):
+def generateNoisyMeasurementsFromGroundTruthData(groundTruthData, measurementNoise=([0,0], [[1000,0],[0,1000]])):
 
-	noisyPositionData = groundTruthData[:,[0,2]] + np.random.multivariate_normal(mean, covariance, len(groundTruthData))
+	noisyPositionData = groundTruthData[:,[0,2]] + np.random.multivariate_normal(measurementNoise[0], measurementNoise[1], len(groundTruthData))
 	
 	noisyMeasurement = np.zeros((501,4))
 	for i, noisyPosition in enumerate(noisyPositionData[0:len(groundTruthData)-1,:]):
@@ -111,26 +195,42 @@ def generateNoisyMeasurementsFromGroundTruthData(groundTruthData, mean=[0,0], co
 
 def plotGroundTruthAndNoisyMeasureMentData():
 
-	groundTruthData = generateGroundTruth()
-	noisyData = generateNoisyMeasurementsFromGroundTruthData(groundTruthData)
+	# groundTruthData = generateGroundTruth()
+	# noisyData = generateNoisyMeasurementsFromGroundTruthData(groundTruthData)
+
+	groundTruthData, noisyPositionData, xhat, z, z_velocity = generateDataWithMatrices()
+
+
+	#print(noisyPositionData)
+
+	noisyMeasurement = np.zeros((501,4))
+	for i, noisyPosition in enumerate(noisyPositionData[0:len(groundTruthData)-1,:]):
+		noisyMeasurement[i][0] = noisyPositionData[i][0]
+		noisyMeasurement[i][1] = noisyPositionData[i+1][0] - noisyPositionData[i][0]
+		noisyMeasurement[i][2] = noisyPositionData[i][1]
+		noisyMeasurement[i][3] = noisyPositionData[i+1][1] - noisyPositionData[i][1]
+
+	noisyMeasurement[len(groundTruthData)-1][0] = noisyPositionData[len(groundTruthData)-1][0]
+	noisyMeasurement[len(groundTruthData)-1][2] = noisyPositionData[len(groundTruthData)-1][1]
 
 	plt.figure()
 	plt.plot(groundTruthData[:,0], groundTruthData[:,2])
-	plt.plot(noisyData[:,0], noisyData[:,2])
+	plt.plot(noisyMeasurement[:,0], noisyMeasurement[:,2])
 
 	plt.figure()
 	plt.plot(list(range(len(groundTruthData))), groundTruthData[:,1])
-	plt.plot(list(range(len(groundTruthData))),  noisyData[:,1])
+	plt.plot(list(range(len(groundTruthData))),  noisyMeasurement[:,1])
 
 	plt.figure()
 	plt.plot(list(range(len(groundTruthData))), groundTruthData[:,3])
-	plt.plot(list(range(len(groundTruthData))),  noisyData[:,3])
+	plt.plot(list(range(len(groundTruthData))),  noisyMeasurement[:,3])
 
 	plt.show()
 
 
 
 if __name__ == "__main__":
+	#plotGroundTruth()
 	plotGroundTruthAndNoisyMeasureMentData()
 
 
