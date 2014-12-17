016bd9b32cf8b3497f68244cfeccc26c9e5d6b40
James <james.kizer@gmail.com>
2014-12-17 14:24:46 -0500

    final updates

---
+++ Question1.py
@@ -0,0 +1,4 @@
+import simulation as s
+
+if __name__ == "__main__":
+  s.plotGroundTruthAndNoisyMeasureMentData()
--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 import random
+import numpy as np
 
 print (random.random())
+print (np.identity(2)*89)
+
+accel_variance = 1
+pooo = np.array([m.T for m in np.matrix(np.random.multivariate_normal([0,0], np.identity(2)*accel_variance, 10))])
+
+print (pooo)
+
+xhat = np.empty(10, dtype=np.dtype(object))
+print (xhat)
